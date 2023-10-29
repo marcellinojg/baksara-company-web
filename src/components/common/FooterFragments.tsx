6fc970beaa7c2f0481fda6fdba80a8ff1e8062b0
marcellinojg <c14200043@john.petra.ac.id>
2023-10-29 19:36:18 +0700

    Fix download animation damp

--- src/components/common/FooterFragments.tsx
+++ src/components/common/FooterFragments.tsx
@@ -9,7 +9,7 @@ export const FooterDownload = () => {
     return <div id="downloadApp" className=" text-black dark:text-white flex justify-center lg:flex-row flex-col items-center gap-[5vw] pt-20 ">
 
         <div className="lg:w-4/12 md:w-2/3 w-10/12 flex flex-col gap-5">
-            <Fade cascade damping={0.5}>
+            <Fade cascade damping={0.1}>
                 <h1 className="font-bold font-family-secondary md:text-5xl text-4xl w-11/12">{translate('Unduh sekarang di App Store dan Play Store.')}</h1>
                 <span className="md:text-lg text-black dark:text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, quo. [6]</span>
                 <div className="flex items-center gap-2">
