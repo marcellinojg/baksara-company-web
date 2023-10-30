5dfd757b3af0d377b5fe1f36c560cfb52ff36ed2
marcellinojg <c14200043@john.petra.ac.id>
2023-10-30 16:38:44 +0700

    Add jumbotron

--- src/pages/external/Faq.tsx
+++ src/pages/external/Faq.tsx
@@ -1,9 +1,12 @@
+import Jumbotron from "../../components/common/Jumbotron"
 import ExternalLayout from "../layouts/ExternalLayout"
 
 
 const FaqPage = () => {
     return <ExternalLayout>
-        <></>
+        <Jumbotron>
+            <span>Frequently <br /> Asked<br /> Questions</span>
+        </Jumbotron>
     </ExternalLayout>
 }
 
--- src/pages/external/News.tsx
+++ src/pages/external/News.tsx
@@ -1,9 +1,16 @@
+import Jumbotron from "../../components/common/Jumbotron"
 import ExternalLayout from "../layouts/ExternalLayout"
 
 
 const NewsPage = () => {
     return <ExternalLayout>
-        <></>
+        <Jumbotron>
+            <span>Baksara<br /> News</span>
+        </Jumbotron>
+        <div className="min-w-screen text-primary dark:text-white py-16 transition duration-300 w-10/12 mx-auto">
+            <h1 className="font-bold font-family-secondary lg:text-5xl text-3xl">Lorem, ipsum. [17]</h1>
+
+        </div>
     </ExternalLayout>
 }
 
