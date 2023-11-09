d91e00f9cf8fee979b32e9f3ee152ceb382bba69
marcellinojg <c14200043@john.petra.ac.id>
2023-11-09 15:01:02 +0700

    minor margin fix

--- src/components/common/Accordion.tsx
+++ src/components/common/Accordion.tsx
@@ -13,11 +13,11 @@ const Accordion = (props: AccordionProps) => {
     return <>
         <div className="w-full border-b-2 border-primary dark:text-primary-dark dark:border-primary-dark">
             <button type="button" onClick={() => setIsActive(prev => !prev)} className="flex py-4 px-2 w-full justify-between items-center cursor-pointer">
-                <h2>{title}</h2>
+                <h2 className="text-xl font-semibold">{title}</h2>
                 <span className={`text-primary dark:text-primary-dark transition duration-300 ${isActive === true ? 'rotate-180' : 'rotate-0'}`}><FaChevronDown/></span>
             </button>
             <div className={`w-full overflow-hidden transition-all ease-linear duration-300 ${isActive === true ? 'max-h-[350px]' : 'max-h-0'}`}>
-                <p className="my-4 mx-2 text-justify ">{content}</p>
+                <p className="mx-2 mb-4 text-justify ">{content}</p>
             </div>
         </div>
     </>
