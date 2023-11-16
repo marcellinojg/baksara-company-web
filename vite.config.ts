2b20cd9980b07afd9a8a5867123135921d8cf96a
marcellinojg <c14200043@john.petra.ac.id>
2023-11-16 09:24:07 +0700

    Fix open graph meta tags

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import ogPlugin from 'vite-plugin-open-graph';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ogPlugin({
+      basic: {
+        title: 'Baksara Indonesia',
+        description: 'Baksara Adalah 123',
+        image: {
+          url: 'https://www.baksara.marcellinojg.site/images/app_icon.png',
+          secureUrl: 'https://www.baksara.marcellinojg.site/images/app_icon.png',
+          type: 'image/png',
+          width: 1080,
+          height: 1080,
+        }
+      },
+      twitter: {
+        card: 'summary',
+        title: 'Baksara Indonesia',
+        description: 'Baksara Adalah 123',
+        image: 'https://www.baksara.marcellinojg.site/images/app_icon.png',
+      },
+    }),
+  ],
 })
