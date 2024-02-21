e4efa891a3dd4e07736fdd16b536c8cc7cfc5a05
marcellinojg <c14200043@john.petra.ac.id>
2024-02-21 20:32:02 +0700

    add google playstore link

--- src/components/common/DownloadAppLink.tsx
+++ src/components/common/DownloadAppLink.tsx
@@ -5,7 +5,7 @@ const DownloadAppLink = () => {
         <a href="https://www.google.com" target="_blank">
             <img src="/images/icon-ios.webp" alt="Download App Store" width={150} />
         </a>
-        <a href="https://www.google.com" target="_blank">
+        <a href="https://play.google.com/store/apps/details?id=com.baksaraid.baksara" target="_blank">
             <img src="/images/icon-gplay.webp" alt="Download Google Play Store" width={150} />
         </a>
     </div>
